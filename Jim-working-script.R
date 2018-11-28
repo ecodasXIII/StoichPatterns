e4c04883216c742adc7b3aedc0a9bd62ca7ba477
jimjunker1 <james.junker1@gmail.com>
2018-11-28 16:05:00 -0700

    pulled all data for lakes. ID'd correct files and variables.

--- Jim-working-script.R
+++ Jim-working-script.R
@@ -1,12 +1,59 @@
 library(tidyverse)
+# run once to download all the data locally to './raw-data/' folder
+# which is ignored for pushing purposes so csvs are not kept remotely
+# just locally
+
 #source("./data-download.Rmd")
+####
+nars_data = read_csv("data/nars_data_table.csv");nars_data$filename <- basename(nars_data$data_link)
+#####
+#check the data surveys available
+nars_data %>%
+  pull(Survey) %>% unique()
 
-nars_data = read_csv("data/nars_data_table.csv")
+### Lakes data
+#check the indicator data available
+nars_data %>%
+  filter(Survey == "Lakes 2012") %>%
+  pull(Indicator) %>% unique()
 
+#check Water Chemistry available
+lakes2012 <- nars_data %>%
+  filter(Survey == "Lakes 2012", 
+         Indicator == "Water Chemistry") %>%
+  pull(filename) # 2 files
+
+#first water chemistry file
+lakes2012_wc1 <- nars_data %>%
+  filter(Survey == "Lakes 2012", 
+         Indicator == "Water Chemistry") %>%
+  pull(filename) %>% '[['(1) %>% #This is site data
+  paste0("raw-data/",.) %>% read_csv() 
+
+# second water chemistry file
+lakes2012_wc2 <- nars_data %>%
+  filter(Survey == "Lakes 2012", 
+         Indicator == "Water Chemistry") %>%
+  pull(filename) %>% '[['(2) %>% #This is actual chemistry data
+  paste0("raw-data/",.) %>% read_csv()
+
+# site information file
+lakes2012_site = nars_data %>%
+  filter(Survey == "Lakes 2012",
+         Indicator == "Site Information") %>%
+  pull(filename) %>% paste0("raw-data/",.) %>% read_csv()
+
+#This unzips shapefile & data for lake basins.
+lakes2012_landscape= nars_data %>% #this is GIS file (Shapes, etc.)
+  filter(Survey == "Lakes 2012",
+         Indicator == "Landscape Data") %>%
+  pull(filename) %>% '[[' (2) %>% #both 1 & 2 have sites and lake shapes
+  paste0("raw-data/",.) %>% unzip() 
+
+######## Code for other ecosystems #######
 nars_data %>% 
   filter(Survey == "Wetlands 2011") %>%
   pull(Indicator) %>% unique()
-nars_data$filename <- basename(nars_data$data_link)
 
 
 w2011_wc <- nars_data %>% 
@@ -35,5 +82,4 @@ rivers2008_wc <- nars_data %>%
   head(1) %>% # because there are 2 files.. 
   paste0("data/", .) %>%
   read_csv()
-
-lakes
+########################################
