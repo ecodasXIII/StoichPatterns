4a13c379f125b6e8221506262273a96303ef4899
Whitney Beck <wbeck1990@gmail.com>
2019-03-06 11:59:26 -0700

    Made a couple changes to make the script more generalizable

--- PRISM Script.R
+++ PRISM Script.R
@@ -4,12 +4,14 @@ library(raster)
 library(zoo)
 
 #Get site locations and sample dates from a specified dataset
-streams <- read.csv("data/siteinfo_0.csv", header = TRUE)
+SiteData <- read.csv("data/assessed_ncca2010_siteinfo.revised.06212016.csv", header = TRUE)
 
 #may have to change date "format" depending on how dates are formatted in csv
-streams$date <- as.Date(streams$DATE_COL, format = "%d-%b-%y") 
-streams$lon <- streams$LON_DD83
-streams$lat <- streams$LAT_DD83
+SiteData$date <- as.Date(SiteData$DATE_COL, format = "%d-%b-%y") 
+SiteData$lon <- SiteData$ALON_DD
+SiteData$lat <- SiteData$ALAT_DD
+
+SiteData=SiteData[!is.na(SiteData$date), ]
 
 #Set a location where all the PRISM files will be saved
 options(prism.path = "prism/PRISM_temp_daily")
@@ -20,21 +22,21 @@ options(prism.path = "prism/PRISM_temp_daily")
 #get_prism_monthlys is the option for monthly data
 #get_prism_annual is the option for annual data
 
-get_prism_dailys(type ="tmean", dates = unique(streams$date), keepZip = FALSE)
+get_prism_dailys(type ="tmean", dates = unique(SiteData$date), keepZip = FALSE)
 
 #Loop through all the date points we want to get temperature data for
-for (i in 1:nrow(streams)){
+for (i in 1:nrow(SiteData)){
   
   #These lines get the right file name for the sample date
-  dt <- gsub("-", "", streams$date[i])
+  dt <- gsub("-", "", SiteData$date[i])
   file_nm <- grep(dt, ls_prism_data()[,1], value = T)
   file_nm <- grep("tmean", file_nm, value = T)
   
   #This pulls the data from the file for the lon/lat point you supply
-  data <- prism_slice(c(streams$lon[i], streams$lat[i]), file_nm)
+  data <- prism_slice(c(SiteData$lon[i], SiteData$lat[i]), file_nm)
   
   #It returns a list so you have to specify the datapoint you want
   #Adds a column to the original dataset with the mean daily temperature
-  streams$tmean_day[i] <- data$data$data[1]
+  SiteData$tmean_day[i] <- data$data$data[1]
 }
 
